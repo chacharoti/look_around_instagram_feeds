8a1b877eacc43e58cf6c76c1ae3893b1df97e2c4
chacharoti <chacharoti@gmail.com>
2015-10-15 18:03:21 +0700

    Update InstagramController#look_around: get data from Instagram API

--- app/controllers/instagram_controller.rb
+++ app/controllers/instagram_controller.rb
@@ -1,5 +1,18 @@
 class InstagramController < ApplicationController
-  def look_around
 
+  def look_around
+    instagram_request = Instagram.new( latitude, longitude )
+    result = instagram_request.media_search
+    @news_feeds = JSON.parse(result.body)['data']
+  end
+
+  private
+
+  def latitude
+    params.require(:latitude)
+  end
+
+  def longitude
+    params.require(:longitude)
   end
 end
