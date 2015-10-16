b57ef91e9fba59b21333df35ed8be6c10b91536e
chacharoti <chacharoti@gmail.com>
2015-10-16 15:55:27 +0700

    Update Instagram Class: Show Error message if params are not valid

--- app/controllers/instagram_controller.rb
+++ app/controllers/instagram_controller.rb
@@ -1,22 +1,28 @@
 class InstagramController < ApplicationController
 
   def look_around
-    instagram_request = Instagram.new( latitude, longitude, radius )
-    result = instagram_request.media_search
-    @news_feeds = JSON.parse(result.body)['data']
+    instagram = Instagram.new( latitude, longitude, radius )
+
+    if instagram.instance_values.present?
+      data_response = instagram.media_search
+      @news_feeds = JSON.parse(data_response.body)['data']
+
+    else
+      @error = 'Your latitude, longitude or radius are not valid. Please check'
+    end
   end
 
   private
 
   def latitude
-    params.require(:latitude)
+    params['latitude']
   end
 
   def longitude
-    params.require(:longitude)
+    params['longitude']
   end
 
   def radius
-    params.require(:radius)
+    params['radius']
   end
 end
--- app/services/instagram.rb
+++ app/services/instagram.rb
@@ -1,15 +1,30 @@
-class Instagram
+class Instagram 
   include HTTParty
   base_uri 'https://api.instagram.com'
 
-  def initialize(latitude, longitude, radius)
-    @options = { query: { lat: latitude, 
-                          lng: longitude, 
-                          distance: radius,
-                          access_token: ENV['INSTAGRAM_ACCESS_TOKEN'] } }
+  def initialize(latitude, longitude, radius)    
+    if  check_valid_of? latitude, longitude, radius
+      @options = { query: { lat: latitude, 
+                            lng: longitude, 
+                            distance: radius,
+                            access_token: ENV['INSTAGRAM_ACCESS_TOKEN'] } }
+    end
   end
 
   def media_search
     self.class.get("/v1/media/search", @options)
+  end
+
+  def check_valid_of? latitude, longitude, radius
+    if  latitude.valid_float? &&
+        latitude.to_f.between?( -90, 90 ) &&
+        longitude.valid_float? &&
+        longitude.to_f.between?( -180, 180 ) &&
+        radius.valid_float? &&
+        radius.to_f.between?( 1, 5000 ) 
+      return true
+    else 
+      return false
+    end
   end
 end
